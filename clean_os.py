828002dab4d742cdf368d07487b30dfae85c71b1
dneial <danimbc@gmail.com>
2023-10-23 15:06:12 +0200

    osssssss

---
+++ clean_os.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+file = "./Comparison_of_operating_systems_1/Comparison_of_operating_systems_1_raw.csv"
+df = pd.read_csv(file)
+
+oss = ["Linux", "Windows (NT family)", "Mac OS Classic"]
+df = df[df["Name"].isin(oss)]
+
+features = ["Computer architecture supported", "Kernel type", "File system supported"]
+
+cols_todrop = [col for col in df.columns if col not in features]
+print(cols_todrop)
+df = df.drop(cols_todrop, axis=1)
+
+df = pd.get_dummies(df, columns=features, dtype=int)
+# df = df.drop(df.columns[df.apply(lambda n: not n.isin(features))], axis=1)
+
+
+print(df.head())
+df.to_csv("./Comparison_of_operating_systems_1/clean_os.csv")
