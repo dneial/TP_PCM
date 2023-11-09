362ea71d97de0dfb36b851f7b7c87f8e18566549
dneial <danimbc@gmail.com>
2023-11-09 16:24:19 +0100

    relations

--- clean_License.py
+++ clean_License.py
@@ -2,53 +2,56 @@
 
 import pandas as pd
 
-file = "Licenses/Comparison_of_free_and_open-source_software_licenses_1_raw.csv"
-df = pd.read_csv(file)
+def gen_licence_fc():
+    file = "Licenses/Comparison_of_free_and_open-source_software_licenses_1_raw.csv"
+    df = pd.read_csv(file)
 
-#Licenses dans Soft : 
-    # Apache                                    ok
-    # Apache License                            ok
-    # GPL                                       ?
-    # GPL2                                      ok
-    # other proprietary                         ?
-    # MPL derivative with badgeware clause      ok
-    # AGPL                                      ok
-    # CPAL                                      ?
+    #Licenses dans Soft :
+        # Apache                                    ok
+        # Apache License                            ok
+        # GPL                                       ?
+        # GPL2                                      ok
+        # other proprietary                         ?
+        # MPL derivative with badgeware clause      ok
+        # AGPL                                      ok
+        # CPAL                                      ?
 
-# Match dans licenses.csv :
+    # Match dans licenses.csv :
 
-    # "Apache License 2.0"                        Apache License 
+        # "Apache License 2.0"                        Apache License
 
-    # "GNU Lesser General Public License"         GPL
-    # "GNU General Public License v2"             GPL2
+        # "GNU Lesser General Public License"         GPL
+        # "GNU General Public License v2"             GPL2
 
-    # "GNU Affero General Public License"         AGPL
-    
-    # "Mozilla Public License 2.0"                MPL
+        # "GNU Affero General Public License"         AGPL
 
-    # "Common Public License"                     CPAL
+        # "Mozilla Public License 2.0"                MPL
+
+        # "Common Public License"                     CPAL
 
 
-li = ["Apache License 2.0","GNU Lesser General Public License","GNU General Public License v2","GNU Affero General Public License","Mozilla Public License 2.0","Common Public License"]
-df = df[df["License and version"].isin(li)]
+    li = ["Apache License 2.0","GNU Lesser General Public License","GNU General Public License v2","GNU Affero General Public License","Mozilla Public License 2.0","Common Public License"]
+    df = df[df["License and version"].isin(li)]
 
-features = ["FSF approval","GPL (v3) compatibility","OSI approval","Copyfree approval","Debian approval","Fedora approval"]
+    features = ["FSF approval","GPL (v3) compatibility","OSI approval","Copyfree approval","Debian approval","Fedora approval"]
 
-df = pd.get_dummies(df, columns=features, dtype=int)
-# df = df.drop(df.columns[df.apply(lambda n: not n.isin(features))], axis=1)
+    df = pd.get_dummies(df, columns=features, dtype=int)
+    # df = df.drop(df.columns[df.apply(lambda n: not n.isin(features))], axis=1)
 
-# rennommage des noms pour correspondre à la table soft
-df.replace("Apache License 2.0", "Apache License", inplace=True)
-df.replace("GNU Lesser General Public License", "GPL", inplace=True)
-df.replace("GNU General Public License v2", "GPL2", inplace=True)
-df.replace("GNU Affero General Public License", "AGPL", inplace=True)
-df.replace("Mozilla Public License 2.0", "MPL", inplace=True)
-df.replace("Common Public License", "CPAL", inplace=True)
+    # rennommage des noms pour correspondre à la table soft
+    df.replace("Apache License 2.0", "Apache License", inplace=True)
+    df.replace("GNU Lesser General Public License", "GPL", inplace=True)
+    df.replace("GNU General Public License v2", "GPL2", inplace=True)
+    df.replace("GNU Affero General Public License", "AGPL", inplace=True)
+    df.replace("Mozilla Public License 2.0", "MPL", inplace=True)
+    df.replace("Common Public License", "CPAL", inplace=True)
 
-# rennommage des valeurs pour correspondre au format binaire demandé
-df.replace("Yes", 1, inplace=True)
-df.replace("Partial", 1, inplace=True)
-df.replace("No", 0, inplace=True)
+    # rennommage des valeurs pour correspondre au format binaire demandé
+    df.replace("Yes", 1, inplace=True)
+    df.replace("Partial", 1, inplace=True)
+    df.replace("No", 0, inplace=True)
 
-print(df.head())
-df.to_csv("./Cleaned/clean_license.csv", index=False)
+    df.to_csv("./Cleaned/clean_license.csv", index=False)
+
+if __name__ == "__main__":
+    gen_licence_fc()
--- clean_os.py
+++ clean_os.py
@@ -2,26 +2,32 @@
 
 import pandas as pd
 
-file = "./OS/Comparison_of_operating_systems_1_raw.csv"
-df = pd.read_csv(file)
 
-oss = ["Linux", "Windows (NT family)", "Mac OS Classic"]
-df = df[df["Name"].isin(oss)]
+def gen_os_fc(file: str):
+    df = pd.read_csv(file)
 
-features = ["Name", "Computer architecture supported", "Kernel type", "File system supported"]
+    oss = ["Linux", "Windows (NT family)", "Mac OS Classic"]
+    df = df[df["Name"].isin(oss)]
 
-cols_todrop = [col for col in df.columns if col not in features]
-df = df.drop(cols_todrop, axis=1)
+    features = ["Name", "Computer architecture supported", "Kernel type", "File system supported"]
 
-df2 = df["File system supported"].str.get_dummies(sep=",")
-df3 = df["Computer architecture supported"].str.get_dummies(sep=",")
+    cols_todrop = [col for col in df.columns if col not in features]
+    df = df.drop(cols_todrop, axis=1)
 
-df.drop("File system supported", inplace=True, axis=1)
-df.drop("Computer architecture supported", inplace=True, axis=1)
+    df2 = df["File system supported"].str.get_dummies(sep=",")
+    df3 = df["Computer architecture supported"].str.get_dummies(sep=",")
 
-df = pd.get_dummies(df, columns=["Kernel type"], dtype=int)
-df = pd.concat([df, df2, df3], axis=1, join="inner")
+    df.drop("File system supported", inplace=True, axis=1)
+    df.drop("Computer architecture supported", inplace=True, axis=1)
 
-df.replace("Windows (NT family)", "Windows", inplace=True)
-df.replace("Mac OS Classic", "Mac OS", inplace=True)
-df.to_csv("./Cleaned/clean_os.csv", index=False)
+    df = pd.get_dummies(df, columns=["Kernel type"], dtype=int)
+    df = pd.concat([df, df2, df3], axis=1, join="inner")
+
+    df.replace("Windows (NT family)", "Windows", inplace=True)
+    df.replace("Mac OS Classic", "Mac OS", inplace=True)
+    df.to_csv("./Cleaned/clean_os.csv", index=False)
+
+
+if __name__ == "__main__":
+    file = "./OS/Comparison_of_operating_systems_1_raw.csv"
+    gen_os_fc(file)
